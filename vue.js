62f8c7f55301e40b6f00c597832fc225a2508265
John Davis <103592957+johndavis1999@users.noreply.github.com>
2022-11-24 16:04:11 -0500

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,18 @@
+var vm = new Vue({
+	el: '#misTareas',
+	data: {
+		nuevaTarea: '',
+		tareas: [],
+	},
+	methods: {
+		addTarea: function() {
+			if (this.nuevaTarea!='')
+			this.tareas.push({ text: this.nuevaTarea, completada: false})
+			this.nuevaTarea='';
+		},
+		compTarea: function(tarea){
+			tarea.completada=!tarea.completada;
+		}
+
+	}
+})
